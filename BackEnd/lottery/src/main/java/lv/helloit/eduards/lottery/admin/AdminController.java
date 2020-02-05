f6f51fe069a232f6040d8d6e7542e1d5aea00aeb
eduard_blumental <eduard.blumental@gmail.com>
2020-02-05 23:00:53 +0200

    Added HTMLs to the project as JSP files

---
+++ BackEnd/lottery/src/main/java/lv/helloit/eduards/lottery/admin/AdminController.java
@@ -0,0 +1,16 @@
+package lv.helloit.eduards.lottery.admin;
+
+
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@XSlf4j
+public class AdminController {
+
+    @GetMapping
+
+
+
+}
