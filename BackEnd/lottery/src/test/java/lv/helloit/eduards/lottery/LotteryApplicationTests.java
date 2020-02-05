41e70f10e0d2f562132e1dc69604c7db3b867c4f
eduard_blumental <eduard.blumental@gmail.com>
2020-02-05 18:19:10 +0200

    BackEnd development started

---
+++ BackEnd/lottery/src/test/java/lv/helloit/eduards/lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.helloit.eduards.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
